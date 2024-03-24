c844ea6f3e2bf5209444a69daf757d29b0aa2e39
ymahmoodSIUE <123699824+ymahmoodSIUE@users.noreply.github.com>
2024-03-24 00:36:43 -0500

    Create sortFirst.sh

---
+++ sortFirst.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <input_file> <output_file>"
+    exit 1
+fi
+
+input_file=$1
+output_file=$2
+
+
+awk '/^>/ { if (seq) { print length(seq), seq; } seq=""; print; next } { seq = seq $0 } END { if (seq) print length(seq), seq; }' "$input_file" > "$output_file"
+
+echo "Filtered sequence lengths and numbers saved to $output_file"
